84046ecfa345930bc19fe7075a545d56333afc88
qJasonm <117887620+qJasonm@users.noreply.github.com>
2023-04-09 23:45:05 -0600

    Update main.py

--- main.py
+++ main.py
@@ -23,6 +23,7 @@ class ImageSorter(tk.Tk):
         self.img_idx = -1
         self.img_list = []
         self.bboxes = []
+        self.value = 0
         self.x = None
         self.y = None
         self.rectangle = None
@@ -50,6 +51,7 @@ class ImageSorter(tk.Tk):
 
         self.protocol("WM_DELETE_WINDOW", self.on_closing)
 
+    
     def create_buttons(self):
         self.day_night_var = tk.IntVar(value=1)
         self.snow_var = tk.IntVar(value=0)
@@ -61,6 +63,7 @@ class ImageSorter(tk.Tk):
         self.no_snow_button = tk.Radiobutton(self, text="No Snow", variable=self.snow_var, value=0)
         self.object_button = tk.Checkbutton(self, text="Object", variable=self.object_var)
         self.obj_class_entry = tk.Entry(self, textvariable=self.obj_class_var)
+        self.prev_button = tk.Button(self, text="Prev", font=("Arial", 14), command=self.show_prev_image)
 
         self.day_button.place(x=0,y=700)
         self.night_button.place(x=85,y=700)
@@ -68,6 +71,7 @@ class ImageSorter(tk.Tk):
         self.no_snow_button.pack(side=tk.LEFT)
         self.object_button.place(x=10,y=850)
         self.obj_class_entry.place(x=10,y=900)
+        self.prev_button.place(x=55, y=370)
 
         self.next_button = tk.Button(self, text="Next", font=("Arial", 14),command=self.save_and_next)
         self.next_button.place(x=55,y=470)
@@ -75,25 +79,27 @@ class ImageSorter(tk.Tk):
         self.reset_button = tk.Button(self,text="Reset", font=("Arial", 14),command=self.reset_button)
         self.reset_button.place(x=46,y=550)
 
+    
     def load_images(self):
         folder_path = filedialog.askdirectory(title="Select Image Folder")
         self.img_list = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith(".jpg")]
         self.load_progress()
 
+    
     def load_progress(self):
         if os.path.exists("progress.csv"):
-            with open("progress.csv", "r") as f:
-                reader = csv.reader(f)
-                for row in reader:
-                    img_path = row[0]
-                    if img_path in self.img_list:
-                        self.img_list.remove(img_path)
+            data = self.saved_data()
+            for row in data:
+                if row[0] in self.img_list:
+                    self.img_list.remove(row[0])
 
+                    
     def save_progress(self, data):
-        with open("progress.csv", "a") as f:
+        with open("progress.csv", 'a', newline='') as f:
             writer = csv.writer(f)
             writer.writerow(data)
 
+    
     def show_next_image(self):
         self.img_idx += 1
         if self.img_idx < len(self.img_list):
@@ -103,6 +109,7 @@ class ImageSorter(tk.Tk):
         else:
             self.canvas.create_text(400, 300, text="All images sorted!", font=("Arial", 24), fill="red")
 
+    
     def display_image(self):
         img = cv2.cvtColor(self.current_img, cv2.COLOR_BGR2RGB)
 
@@ -122,6 +129,7 @@ class ImageSorter(tk.Tk):
         self.canvas.create_image(0, 0, anchor=tk.NW, image=img)
         self.canvas.image = img
 
+    
     def mouse_move_label(self,event):
         self.canvas.lift(self.h_line)
         self.canvas.lift(self.v_line)
@@ -129,19 +137,13 @@ class ImageSorter(tk.Tk):
         self.canvas.coords(self.h_line, 0, y, self.canvas.winfo_width(), y)
         self.canvas.coords(self.v_line, x, 0, x, self.canvas.winfo_height())
 
+    
     def on_left_click(self, event):
         if event.num == 1:  # Check if the left mouse button was clicked
             self.x, self.y = event.x, event.y
             self.rectangle = self.canvas.create_rectangle(self.x,self.y,1,1,outline='red',width=2)
-            # if len(self.bboxes) ==0:
             self.bboxes.append((self.x, self.y))
-            
-            # if len(self.bboxes) == 0:
-            #     self.bboxes.append((x, y))
-            # else:
-            #     x1, y1 = self.bboxes[0]
-            #     self.bboxes.append((x, y))
-            #     self.canvas.create_rectangle(x1, y1, x, y, outline="red", width=2)
+    
     
     def on_left_click_drag(self,event):
         x1, y1 = event.x, event.y
@@ -149,26 +151,60 @@ class ImageSorter(tk.Tk):
         self.canvas.coords(self.h_line, 0, y1, self.canvas.winfo_width(), y1)
         self.canvas.coords(self.v_line, x1, 0, x1, self.canvas.winfo_height())
 
+    
     def on_left_click_release(self, event):
         x1, y1 = event.x, event.y
         self.bboxes.append((x1,y1))
-    
-
+        self.object_var.set(1)
         
+    
     def on_right_click(self, event):
         if event.num == 3:  # Check if the right mouse button was clicked
             x, y = event.x, event.y
-            # for idx, bbox in enumerate(self.bboxes):
-            #     if bbox[0] - 10 <= x <= bbox[0] + 10 and bbox[1] - 10 <= y <= bbox[1] + 10:
-            #         self.bboxes.pop(idx)
-            #         break
             self.bboxes = []
+            self.object_var.set(0)
             
             self.display_image()
 
+    
     def reset_button(self):
         self.bboxes = []
         self.display_image()
+
+    
+    def show_prev_image(self):
+        self.img_idx -= 1
+
+        # get the save data, find the one is showing, remove it from the csv
+        saved_data = self.saved_data()
+        for row in saved_data:
+            if row[0] == self.img_list[self.img_idx]:
+                saved_data.remove(row)
+        with open("progress.csv",'w',newline='') as f:
+            writer = csv.writer(f)
+            writer.writerows(saved_data)
+
+
+        if self.img_idx >= 0:
+            self.current_img_path = self.img_list[self.img_idx]
+            self.current_img = cv2.imread(self.current_img_path)
+            self.display_image()
+        else:
+            self.img_idx = -1
+
+    
+    def saved_data(self):
+        changes = []
+        with open("progress.csv",'r') as f:
+            reader = csv.reader(f)
+            data = list(reader)
+            for i in data:
+                changes.append(i)
+        return changes
+        # with open("progress.csv",'w',newline='') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerows(changes)
+
 
     def save_and_next(self):
         day_night = self.day_night_var.get()
@@ -184,6 +220,7 @@ class ImageSorter(tk.Tk):
         self.obj_class_var.set("")  # Clear objClass variable
         self.show_next_image()
 
+    
     def on_closing(self):
         self.destroy()
 
